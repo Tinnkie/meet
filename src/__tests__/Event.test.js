04528bb27e8cf325836a47ea57ef3fe698c0e54f
Tinnkie <Tinnkie@users.noreply.github.com>
2023-05-10 03:36:11 +0200

    create Event.test.js and test for rendering the component

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { mockData } from "../mock-data";
-import Event from "../Event";
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mockData } from '../mock-data';
+import Event from '../Event';
 
-describe("<Event /> component", () => {
-
+describe('<Event /> component', () => {
     
+    test('renders the component', () => {
+        const EventWrapper = shallow(<Event events={mockData} />);
+        expect(EventWrapper).toBeDefined();
+      });
+
+
 });
