65b626741f00b1944fecd92eec482e4b1872eff9
Tinnkie <Tinnkie@users.noreply.github.com>
2023-05-10 03:04:20 +0200

    create Event.test.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { mockData } from "../mock-data";
+import Event from "../Event";
+
+describe("<Event /> component", () => {
+
+    
+});
