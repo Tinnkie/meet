79e9ef6c65946d6308cc005bb0280a51058c047b
Tinnkie <Tinnkie@users.noreply.github.com>
2023-05-14 19:03:13 +0200

    change EventWrapper

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -4,9 +4,13 @@ import { mockData } from '../mock-data';
 import Event from '../Event';
 
 describe('<Event /> component', () => {
-    
+    let EventWrapper;
+    const event = mockData[0];
+    beforeAll(() => {
+        EventWrapper = shallow(<Event event={event} />);
+    })
+
     test('renders the component', () => {
-        const EventWrapper = shallow(<Event events={mockData} />);
         expect(EventWrapper).toBeDefined();
       });
 
