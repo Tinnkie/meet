31bbfe8a14ed5388fcebad55789e226c7080e504
Tinnkie <Tinnkie@users.noreply.github.com>
2023-05-14 22:59:01 +0200

    define a new method updateEvents

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents } from './api';
 
 class App extends Component {
   state = {
@@ -22,6 +23,15 @@ class App extends Component {
       </div>
     );
   }
+}
+
+updateEvents = (location) => {
+  getEvents().then((events) => {
+    const locationEvents = events.filter((event) => event.location === location);
+    this.setState({
+      events: locationEvents
+    });
+  });
 }
 
 export default App;
