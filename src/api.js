9b48fba762d7356107a91314058f60bda822b9e9
Tinnkie <Tinnkie@users.noreply.github.com>
2023-06-09 01:05:04 +0200

    remove unused code

--- src/api.js
+++ src/api.js
@@ -52,13 +52,6 @@ const getEvents = async (max_results = 32) => {
     if (!navigator.onLine) {
       const data = localStorage.getItem("lastEvents");
       NProgress.done();
-      console.log("offline data: ", data);
-      return data ? JSON.parse(data).events : [];
-    }
-
-    if (!navigator.onLine) {
-      const data = localStorage.getItem("lastEvents");
-      NProgress.done();
       return data ? JSON.parse(data).events : [];
     }
   
